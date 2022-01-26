18cc29b51c623d40cdcabf41cbb3693fa0525111
JansariDivya <divyajansari2379@gmail.com>
2022-01-26 22:16:03 +0530

    Changes made in new file

--- Lab1.c
+++ Lab1.c
@@ -4,7 +4,10 @@
 
 int main()
    {
+   
+   
 	float base,gross;
+	printf("hello from new branch");
 	printf("Enter your Salary");/*Salary is Entered*/
 	scanf("%f",&base);
 	gross=(1+0.4+0.2)*base; /*Calculates the Gross Salary*/
