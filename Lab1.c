13f9930038a620cd06045df941aa5b97739607b3
JansariDivya <divyajansari2379@gmail.com>
2022-01-26 21:47:32 +0530

    firstcommit

---
+++ Lab1.c
@@ -0,0 +1,13 @@
+/*Problem 1:- Gross Salary Problem*/
+
+#include <stdio.h>
+
+int main()
+   {
+	float base,gross;
+	printf("Enter your Salary");/*Salary is Entered*/
+	scanf("%f",&base);
+	gross=(1+0.4+0.2)*base; /*Calculates the Gross Salary*/
+	printf("Your Gross Salary\n is %f",gross); /*Prints the Gross Salary*/
+	return(0);
+   }
